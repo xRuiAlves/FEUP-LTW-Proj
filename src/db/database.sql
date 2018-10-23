79050f163ca2427f512d11348ed3519f8cac69e0
filipa <filipadurao@gmail.com>
2018-10-23 13:20:53 +0100

    Finished the database's SQL and the .db file

---
+++ src/db/database.sql
@@ -0,0 +1,53 @@
+CREATE TABLE User (
+    user_id         INTEGER PRIMARY KEY,
+    user_username   VARCHAR NOT NULL UNIQUE,
+    user_realname   VARCHAR NOT NULL,
+    user_password   VARCHAR NOT NULL,
+    user_bio        VARCHAR
+);
+
+CREATE TABLE VotableEntity (
+    votable_entity_id   INTEGER PRIMARY KEY
+);
+
+CREATE TABLE Comment (
+    comment_id          INTEGER PRIMARY KEY,
+    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity,
+    comment_content     VARCHAR NOT NULL
+);
+
+CREATE TABLE Story (
+    story_id            INTEGER PRIMARY KEY,
+    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity,
+    story_title         VARCHAR NOT NULL,
+    story_content       VARCHAR NOT NULL,
+    story_creation_date DATE NOT NULL
+);
+
+CREATE TABLE Vote (
+    vote_value          INTEGER NOT NULL CHECK (vote_value = -1 OR vote_value = 1),
+    user_id             INTEGER NOT NULL REFERENCES User,
+    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity,
+    PRIMARY KEY (
+        user_id,
+        votable_entity_id
+    )
+);
+
+CREATE TABLE Creator (
+    user_id             INTEGER NOT NULL REFERENCES User,
+    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity, 
+    PRIMARY KEY (
+        user_id,
+        votable_entity_id
+    )
+);
+
+CREATE TABLE VotableEntityComment (
+    comment_id          INTEGER NOT NULL REFERENCES Comment,
+    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity, 
+    PRIMARY KEY (
+        comment_id,
+        votable_entity_id
+    )
+);
