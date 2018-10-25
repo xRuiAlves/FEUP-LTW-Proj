e8a08b87abb078e95345625d6e97d3c78654177f
filipa <filipadurao@gmail.com>
2018-10-25 09:13:32 +0100

    Added data to the database

---
+++ src/db/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO User (user_id, user_username, user_realname, user_password, user_bio) VALUES (1, 'dicas', 'Rui', 'password1234', 'Gosto muito de pasteis de nata!');
+INSERT INTO User (user_id, user_username, user_realname, user_password, user_bio) VALUES (2, 'dilipaFurao', 'Filipa', 'olaola', 'Gosto de chocolate!');
+INSERT INTO User (user_id, user_username, user_realname, user_password, user_bio) VALUES (3, 'limalimao', 'Henrique', 'naoGostoDeESOF', 'Nao gosto de BDAD');
+INSERT INTO User (user_id, user_username, user_realname, user_password, user_bio) VALUES (4, 'angelico', 'Angelo', 'mexicano', 'salsaTequilla');
+
+INSERT INTO VotableEntity (votable_entity_id) VALUES (1);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (2);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (3);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (4);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (5);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (6);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (7);
+INSERT INTO VotableEntity (votable_entity_id) VALUES (8);
+
+INSERT INTO Comment (comment_id, votable_entity_id, comment_content) VALUES (1, 1, 'Gostei imenso deste texto!');
+INSERT INTO Comment (comment_id, votable_entity_id, comment_content) VALUES (2, 2, 'Que fotos horríveis');
+INSERT INTO Comment (comment_id, votable_entity_id, comment_content) VALUES (3, 3, 'Discordo plenamente!');
+INSERT INTO Comment (comment_id, votable_entity_id, comment_content) VALUES (4, 4, 'Que saudades dessa cidade!');
+
+INSERT INTO Story (story_id, votable_entity_id, story_title, story_content, story_creation_date) VALUES (1, 5, 'Viagem a Paris', 'Paris é mesmo bonito bla bla bla bla bla bla bla bla bla', '20170801');
+INSERT INTO Story (story_id, votable_entity_id, story_title, story_content, story_creation_date) VALUES (2, 6, 'Porto!', ' bla bla bla bla bla bla bla bla bla', '20160427');
+INSERT INTO Story (story_id, votable_entity_id, story_title, story_content, story_creation_date) VALUES (3, 7, 'Alemanha!', ' bla bla bla bla bla bla bla bla bla', '20180823');
+INSERT INTO Story (story_id, votable_entity_id, story_title, story_content, story_creation_date) VALUES (4, 8, 'Espanha!', ' bla bla bla bla bla bla bla bla bla', '20160330');
+
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 1, 1);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (-1, 2, 2);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 3, 1);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (-1, 4, 3);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 1, 4);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (-1, 2, 5);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 3, 4);
+INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (-1, 4, 4);
+
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (1, 1);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (1, 2);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (2, 3);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (2, 4);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (3, 5);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (3, 6);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (4, 7);
+INSERT INTO Creator (user_id, votable_entity_id) VALUES (4, 8);
+
+INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (1, 7);
+INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (2, 7);
+INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (3, 1);
+INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (4, 1);
