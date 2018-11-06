15e4bea2518b5f763b59cb18446d7311df1627a6
xRuiAlves <up201606746@fe.up.pt>
2018-11-06 10:45:33 +0000

    Updated schema with comment parent attribute

--- src/db/data.sql
+++ src/db/data.sql
@@ -12,10 +12,10 @@ INSERT INTO VotableEntity (votable_entity_id, user_id) VALUES (6, 3);
 INSERT INTO VotableEntity (votable_entity_id, user_id) VALUES (7, 4);
 INSERT INTO VotableEntity (votable_entity_id, user_id) VALUES (8, 4);
 
-INSERT INTO Comment (votable_entity_id, comment_content) VALUES (1, 'Gostei imenso deste texto!');
-INSERT INTO Comment (votable_entity_id, comment_content) VALUES (2, 'Que fotos horríveis');
-INSERT INTO Comment (votable_entity_id, comment_content) VALUES (3, 'Discordo plenamente!');
-INSERT INTO Comment (votable_entity_id, comment_content) VALUES (4, 'Que saudades dessa cidade!');
+INSERT INTO Comment (votable_entity_id, parent_entity_id, comment_content) VALUES (1, 7, 'Gostei imenso deste texto!');
+INSERT INTO Comment (votable_entity_id, parent_entity_id, comment_content) VALUES (2, 7, 'Que fotos horríveis');
+INSERT INTO Comment (votable_entity_id, parent_entity_id, comment_content) VALUES (3, 1, 'Discordo plenamente!');
+INSERT INTO Comment (votable_entity_id, parent_entity_id, comment_content) VALUES (4, 1, 'Que saudades dessa cidade!');
 
 INSERT INTO Story (votable_entity_id, story_title, story_content, story_creation_date) VALUES (5, 'Viagem a Paris', 'Paris é mesmo bonito bla bla bla bla bla bla bla bla bla', '20170801');
 INSERT INTO Story (votable_entity_id, story_title, story_content, story_creation_date) VALUES (6, 'Porto!', ' bla bla bla bla bla bla bla bla bla', '20160427');
@@ -37,8 +37,3 @@ INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 1, 6);
 INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 1, 7);
 INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (1, 1, 8);
 INSERT INTO Vote (vote_value, user_id, votable_entity_id) VALUES (-1, 3, 7);
-
-INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (1, 7);
-INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (2, 7);
-INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (3, 1);
-INSERT INTO VotableEntityComment (comment_id, votable_entity_id) VALUES (4, 1);
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS User; 
 CREATE TABLE User (
     user_id         INTEGER PRIMARY KEY,
     user_username   VARCHAR NOT NULL UNIQUE,
@@ -6,16 +7,20 @@ CREATE TABLE User (
     user_bio        VARCHAR
 );
 
+DROP TABLE IF EXISTS VotableEntity; 
 CREATE TABLE VotableEntity (
     votable_entity_id   INTEGER PRIMARY KEY,
     user_id             INTEGER NOT NULL REFERENCES User
 );
 
+DROP TABLE IF EXISTS Comment; 
 CREATE TABLE Comment (
     votable_entity_id   INTEGER PRIMARY KEY REFERENCES VotableEntity,
+    parent_entity_id    INTEGER NOT NULL REFERENCES VotableEntity, 
     comment_content     VARCHAR NOT NULL
 );
 
+DROP TABLE IF EXISTS Story; 
 CREATE TABLE Story (
     votable_entity_id   INTEGER PRIMARY KEY REFERENCES VotableEntity,
     story_title         VARCHAR NOT NULL,
@@ -23,21 +28,13 @@ CREATE TABLE Story (
     story_creation_date DATE NOT NULL
 );
 
+DROP TABLE IF EXISTS Vote; 
 CREATE TABLE Vote (
     vote_value          INTEGER NOT NULL CHECK (vote_value = -1 OR vote_value = 1),
     user_id             INTEGER NOT NULL REFERENCES User,
     votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity,
     PRIMARY KEY (
         user_id,
-        votable_entity_id
-    )
-);
-
-CREATE TABLE VotableEntityComment (
-    comment_id          INTEGER NOT NULL REFERENCES Comment,
-    votable_entity_id   INTEGER NOT NULL REFERENCES VotableEntity, 
-    PRIMARY KEY (
-        comment_id,
         votable_entity_id
     )
 );
