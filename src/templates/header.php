eb6a2d14abf26bbc7c0d7b10726f4996d38793f9
Henrique Lima <henriquelim@hotmail.com>
2018-11-15 09:37:32 +0000

    turned dropdown into side menu

--- src/templates/header.php
+++ src/templates/header.php
@@ -7,7 +7,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    <script src="js/navbar.js"></script>
     <title><?=$_env_website_name?></title>
 </head>
 <body>
@@ -17,17 +20,16 @@
             <h1><?=$_env_website_name?></h1>
             <div id="login_slider">
                 <div class="slider_text">
-                    <div class="left"><?=$_env_login?></div>
-                    <div class="right">Henrique</div>
+                    <div class="left">Henrique</div>
+                    <div class="right"><?=$_env_login?></div>
                 </div>
                 <img src="<?=$_env_default_profile_img ?>"/>
-
-                <ul>
-                    <li><?=$_env_profile?></li>
-                    <li><?=$_env_settings?></li>
-                    <li><?=$_env_logout?></li>
-                </ul>
-            <div>
+            </div>
+            <ul class="dropdown">
+                <li><?=$_env_profile?><i class="fas fa-user"></i></li>
+                <li><?=$_env_settings?><i class="fas fa-cog"></i></li>
+                <li><?=$_env_logout?><i class="fas fa-sign-out-alt"></i></li>
+            </ul>
         </nav>
 
 
