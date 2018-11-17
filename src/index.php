318cda6b894b462d16fc2e05346b6e7e59f93b29
Henrique Lima <henriquelim@hotmail.com>
2018-11-17 16:06:36 +0000

    Started stories front-end development

--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <?php include('./templates/header.php') ?>
 
+<div id="latest-stories" class="stories-container"></div>
 
+<script>
+    window.addEventListener('load', () => {
+        displayStories([sampleStory,{...sampleStory, image:undefined},sampleStory,sampleStory,sampleStory,sampleStory,sampleStory,sampleStory,sampleStory,sampleStory,sampleStory,sampleStory], "latest-stories")
+    });
+</script>
 
 <?php include('./templates/footer.php') ?>
