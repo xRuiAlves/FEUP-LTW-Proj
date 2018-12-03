380fd30407b853109cfd4ca50add451ed3310bbd
filipa <filipadurao@gmail.com>
2018-12-03 17:24:33 +0000

    Added user registering

--- src/js/signUp.js
+++ src/js/signUp.js
@@ -5,37 +5,33 @@ function showSignUpForm(){
         form.innerHTML = 
         `
         <h1>Sign Up</h1>
-        <input type="text" placeholder="Name"/> 
-        <input type="text" placeholder="Username"/>
-        <input type="text" placeholder="Short Bio"/>
+        <input type="text" placeholder="Name" class="Name"/> 
+        <input type="text" placeholder="Username" class="Username"/>
+        <input type="text" placeholder="Short Bio" class="Bio"/>
         <input type="password" placeholder="Password"/>
         <input type="file" name="pic" accept="image/*"/>
         <button>Create Account</button>
         <div class="notification warning"></div>`;
         
-        let nameDOM = form.querySelector('input[type="text"]');
-        let usernameDOM = form.querySelector('input[type="text"]');
-        let bioDOM = form.querySelector('input[type="text"]');
+        let nameDOM = form.querySelector('input[type="text"].Name');
+        let usernameDOM = form.querySelector('input[type="text"].UserName');
+        let bioDOM = form.querySelector('input[type="text"].Bio');
         let passwordDOM = form.querySelector('input[type="password"]');
         let profilePicDOM = form.querySelector('input[type="file"]');
-
-        passwordDOM.addEventListener('keydown', (e) => {
-            if (e.keyCode === 13) submitSignUp(form, resolve);
-        });
         
         form.querySelector('button').addEventListener('click', () => submitSignUp(form, resolve));
 
         ModalHandler.show(form);
 
-        usernameDOM.focus();
+        nameDOM.focus();
     });
 }
 
 function submitSignUp(form, resolve){
-    let nameDOM = form.querySelector('input[type="text"]');
-    let usernameDOM = form.querySelector('input[type="text"]');
-    let bioDOM = form.querySelector('input[type="text"]');
+    let usernameDOM = form.querySelector('input[type="text"].Username');
+    let nameDOM = form.querySelector('input[type="text"].Name');
     let passwordDOM = form.querySelector('input[type="password"]');
+    let bioDOM = form.querySelector('input[type="text"].Bio');
     let profilePicDOM = form.querySelector('input[type="file"]');
 
     let formData = new FormData();
@@ -43,13 +39,13 @@ function submitSignUp(form, resolve){
     formData.append('user_realname', nameDOM.value);
     formData.append('user_password', passwordDOM.value);
     formData.append('user_bio', bioDOM.value);
-    formData.append('usr_img', profilePicDOM.value);
+    formData.append('user_img', profilePicDOM.files[0]);
     
     fetch('api/user/create', 
             {method: "POST",
             body: formData})
     .then(res => {
-        if(res.status === 200){
+        if(res.status === 201){
             ModalHandler.hide();
             return res.json();
         }else{
@@ -60,8 +56,5 @@ function submitSignUp(form, resolve){
         console.log(data)
         document.querySelector('#topbar #login_slider > .slider_text div.left').innerText = data.user_username;
         resolve();
-    })
-    .catch((res) => {
-        form.querySelector('.notification').innerText = 'Could not login: ' + res.statusText;
-    }) */    
+    })*/    
 }
