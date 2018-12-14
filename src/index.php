8452a9a248ac6eaad4726cdeee3288140cb0b9ba
Henrique Lima <henriquelim@hotmail.com>
2018-12-14 14:40:33 +0000

    Added time stamp display to stories and comments

--- src/index.php
+++ src/index.php
@@ -8,19 +8,21 @@
     <img src="images/banner-light.png"/>
 </div>
 <div id="floatingActionButton" class="hidden"><span>+</span><i class="fas fa-pen"></i></div>
-<div class="page-divider"> Latest stories <hr></div>
-<div id="latest-stories" class="stories-container"></div>
-<button id="btn-load-latest">
-    Load More Recent Stories!
-</button>
-<div class="page-divider"> 
-    Most Upvoted stories
-    <hr>
-</div>
-<div id="most-upvoted-stories" class="stories-container"></div>
-<button id="btn-load-most-upvoted">
-    Load More Stories!
-</button>
 
+<div>
+    <div class="page-divider"> Latest stories</div>
+    <div id="latest-stories" class="stories-container"></div>
+    <button id="btn-load-latest">
+        Load More Recent Stories!
+    </button>
+</div>
+
+<div>
+    <div class="page-divider">Most Upvoted stories</div>
+    <div id="most-upvoted-stories" class="stories-container"></div>
+    <button id="btn-load-most-upvoted">
+        Load More Stories!
+    </button>
+</div>
 
 <?php include('./templates/footer.php') ?>
--- src/profile.php
+++ src/profile.php
@@ -4,7 +4,7 @@
 
 <script src="js/profilepage.js" defer type="module"></script>
 
-<div id="floatingActionButton" class="hidden"><span></span><i class="fas fa-pen"></i></div>
+<div id="floatingActionButton" class="hidden"><span>+</span><i class="fas fa-pen"></i></div>
 <div class="card profile-info">
     <img class="pic">
     <p class="name"></p>
@@ -14,17 +14,21 @@
 </div>
 
 <div class="profile-content">
-    <div class="page-divider"> Latest stories <hr></div>
-    <div id="latest-stories" class="stories-container"></div>
-    <button id="btn-load-latest">
-        Load more stories
-    </button>
+    <div>
+        <div class="page-divider"> Latest stories</div>
+        <div id="latest-stories" class="stories-container"></div>
+        <button id="btn-load-latest">
+            Load more stories
+        </button>
+    </div>
     
-    <div class="page-divider"> Most Upvoted stories <hr></div>
-    <div id="upvoted-stories" class="stories-container"></div>
-    <button id="btn-load-most-upvoted">
-        Load more stories
-    </button>
+    <div>
+        <div class="page-divider"> Most Upvoted stories</div>
+        <div id="upvoted-stories" class="stories-container"></div>
+        <button id="btn-load-most-upvoted">
+            Load more stories
+        </button>
+    </div>
 </div>
 
 
