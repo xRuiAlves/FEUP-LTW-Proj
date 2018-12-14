fd5d1cd5c422da5300ffd3bb7a6b177265a2f9a2
Henrique Lima <henriquelim@hotmail.com>
2018-12-14 11:20:17 +0000

    Did floating action button hover action

--- src/index.php
+++ src/index.php
@@ -7,7 +7,7 @@
 <div id="banner">
     <img src="images/banner-light.png"/>
 </div>
-<div id="floatingActionButton" class="hidden"><span>&#43;</span></div>
+<div id="floatingActionButton" class="hidden"><span>+</span><i class="fas fa-pen"></i></div>
 <div class="page-divider"> Latest stories <hr></div>
 <div id="latest-stories" class="stories-container"></div>
 <button id="btn-load-latest">
