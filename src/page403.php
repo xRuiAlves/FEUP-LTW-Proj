1f8718547811747ce7d526bf43dee38073603bcb
xRuiAlves <up201606746@fe.up.pt>
2018-12-15 20:05:44 +0000

    Developed 403 and 404 pages

---
+++ src/page403.php
@@ -0,0 +1,14 @@
+<?php include('./templates/header.php') ?>
+
+<link rel="stylesheet" href="css/page4xx.css">
+
+<section class="error-section">
+    <header class="error-code">
+        403
+    </header>
+    <p>
+        Forbidden: You do not possess the necessary permissions
+    </p>
+</section>
+
+<?php include('./templates/footer.php') ?>
---
+++ src/page404.php
@@ -0,0 +1,14 @@
+<?php include('./templates/header.php') ?>
+
+<link rel="stylesheet" href="css/page4xx.css">
+
+<section class="error-section">
+    <header class="error-code">
+        404
+    </header>
+    <p>
+        Page not found
+    </p>
+</section>
+
+<?php include('./templates/footer.php') ?>
