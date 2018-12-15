553bd7bb984efe3b193bd6023254cdfb172102dd
Rui Pedro Moutinho Moreira Alves <up201606746@gnomo.fe.up.pt>
2018-12-15 20:33:44 +0000

    Updated server config script

--- src/server_config.sh
+++ src/server_config.sh
@@ -6,13 +6,13 @@ if [[ "$#" != 1 ]]; then
 fi
 
 # change root path in .htaccess configs file
-sed "s/ROOT_PATH/$1/g" .htaccess > .htaccess_
+sed "s/ROOT_PATH/$1/" .htaccess > .htaccess_
 mv .htaccess_ .htaccess
 
 # change root path in php environment variables
-sed "s/ROOT_PATH/$1/g" ./templates/env.php > ./templates/env.php_
+sed "s/ROOT_PATH/$1/" ./templates/env.php > ./templates/env.php_
 mv ./templates/env.php_ ./templates/env.php
 
 # change root path in js environment variables
-sed "s/ROOT_PATH/$1/g" ./js/env.js > ./js/env.js_
+sed "s/ROOT_PATH/$1/" ./js/env.js > ./js/env.js_
 mv ./js/env.js_ ./js/env.js
