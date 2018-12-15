a1917c18ee1c1821f5782f008d9a18e87cc8ab7b
Henrique Lima <henriquelim@hotmail.com>
2018-12-15 16:58:23 +0000

    Added Full name to profile page

--- src/profile.php
+++ src/profile.php
@@ -11,6 +11,7 @@
         <i class="fas fa-edit edit-picture-icon" onclick="showProfilePictureChangeForm()"></i>
     </div>
     <p class="name"></p>
+    <p class="username"></p>
     <textarea class="bio" onchange="updateBio()"></textarea>
     <div class="footer">
         <div class="user-points-div">
