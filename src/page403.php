173e0933826c2cf8b1561f3145a39c90de8ed70d
xRuiAlves <up201606746@fe.up.pt>
2018-12-16 10:07:41 +0000

    Changed pages 403 and 404 styling to internal style sheet

--- src/page403.php
+++ src/page403.php
@@ -1,6 +1,23 @@
 <?php include('./templates/header.php') ?>
 
-<link rel="stylesheet" href="css/page4xx.css">
+<style>
+    .error-section {
+        text-align: center;
+    }
+
+    .error-section p {
+        margin: 0;
+        font-size: 2.2em;
+        color: var(--color-grey-dark);
+    }
+
+    .error-code {
+        color: var(--main-color-dark);
+        font-size: 12em;
+        margin-top: 0.5em;
+        font-weight: bold;
+    }
+</style>
 
 <section class="error-section">
     <header class="error-code">
