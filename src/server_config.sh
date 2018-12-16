796706ed8d37e2d41d2fef12cde6eb441d376658
xRuiAlves <up201606746@fe.up.pt>
2018-12-16 22:48:37 +0000

    Updated server configuration file

--- src/server_config.sh
+++ src/server_config.sh
@@ -6,13 +6,13 @@ if [[ "$#" != 1 ]]; then
 fi
 
 # change root path in .htaccess configs file
-sed "s/ROOT_PATH/$1/" .htaccess > .htaccess_
-mv .htaccess_ .htaccess
+mv .htaccess .htaccess.bak
+sed "s/ROOT_PATH/$1/" .htaccess.bak > .htaccess
 
 # change root path in php environment variables
-sed "s/ROOT_PATH/$1/" ./templates/env.php > ./templates/env.php_
-mv ./templates/env.php_ ./templates/env.php
+mv ./templates/env.php ./templates/env.php.bak
+sed "s/ROOT_PATH/$1/" ./templates/env.php.bak > ./templates/env.php
 
 # change root path in js environment variables
-sed "s/ROOT_PATH/$1/" ./js/env.js > ./js/env.js_
-mv ./js/env.js_ ./js/env.js
+mv ./js/env.js ./js/env.js.bak
+sed "s/ROOT_PATH/$1/" ./js/env.js.bak > ./js/env.js
